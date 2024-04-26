b4284968b8e80c2b0b8704b9299a6c353365891e
Kamilla <s_nauruzbayeva23@stud.hwr-berlin.de>
2024-04-26 13:15:25 +0200

    Changed utils.py function

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
 def load_and_filter_female_passengers(filepath):
     data = pd.read_csv(filepath) 
-    female_passengers = data[data['sex'] == 'female']
+    female_passengers = data[data['sex'] == 'male']
     return female_passengers
